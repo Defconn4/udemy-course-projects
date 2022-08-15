4bd7c16ec22423940f02a92f3985ad8a429ab2ab
Defconn4 <fcos624@gmail.com>
2022-08-15 19:27:40 -0400

    up to section 3 - video 38

--- react-the-complete-guide-full-course/react-the-complete-guide/src/index.js
+++ react-the-complete-guide-full-course/react-the-complete-guide/src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App"; // We can omit the `.js` extension since App is one of our own files (relative import path).
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
